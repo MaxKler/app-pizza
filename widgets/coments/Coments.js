d1ced9c25717d73422b2cfc2d1a61fadcae74669
MaxKler <maximumwwe@gmail.com>
2021-11-20 20:51:32 +0200

    d

--- widgets/coments/Coments.js
+++ widgets/coments/Coments.js
@@ -4,8 +4,23 @@ import classes from './../../styles/widgets/coments/coments-style.module.scss'
 import cat from './../../project/image/widgets/coments/cat.png'
 import Frame from './../../project/image/widgets/coments/Frame.png'
 import phone from './../../project/image/widgets/coments/phone.png'
+import instImg from './../../project/image/widgets/coments/instImg.png'
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
 
 const Coments = () => {
+
+    let settings = {
+        arrows: false,
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 5,
+        slidesToScroll: 1
+    }
     return (
     <div className={classes.coments}>
         <div className={classes.coments__blocks}>
@@ -25,9 +40,37 @@ const Coments = () => {
                 </div>
             </div>
         </div>
-        <div>
+        
+        <div className={classes.insta}>
+        <div className={classes.phone}>
             <img src={phone} alt="" />
         </div>
+        <Slider {...settings}>
+        <div>
+            <img src={instImg} alt="" />
+        </div>
+        <div>
+            <img src={instImg} alt="" />
+        </div>
+        <div>
+            <img src={instImg} alt="" />
+        </div>
+        <div>
+            <img src={instImg} alt="" />
+        </div>
+        <div>
+            <img src={instImg} alt="" />
+        </div>
+        <div>
+            <img src={instImg} alt="" />
+        </div>
+        <div>
+            <img src={instImg} alt="" />
+        </div>
+         </Slider>
+        </div>
+      
+        
     </div>
     )
 }
